0a62992780c7cb91563776fbab3681b6bb782724
wasabina67 <wasabina67@gmail.com>
2026-01-25 13:35:59 +0900

    update

--- update_stats.sh
+++ update_stats.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 set -e
-curl -fL "https://github-readme-stats.vercel.app/api?username=wasabina67&show_icons=true&count_private=true&theme=tokyonight&hide_title=false&disable_animations=true" -o stats.svg
-curl -fL "https://github-readme-stats.vercel.app/api/top-langs/?username=wasabina67&layout=compact&langs_count=8&theme=tokyonight&hide_title=false&disable_animations=true" -o top-langs.svg
+curl -fL "https://github-readme-stats.vercel.app/api?username=wasabina67&show_icons=true&theme=tokyonight&disable_animations=true" -o stats.svg
+curl -fL "https://github-readme-stats.vercel.app/api/top-langs/?username=wasabina67&layout=compact&langs_count=8&theme=tokyonight&disable_animations=true" -o top-langs.svg
